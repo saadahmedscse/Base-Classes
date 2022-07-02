a321280ae2e30ebb8510d210297da4fa73aca4a2
Saad <saadahmedscse@gmail.com>
2022-07-02 23:24:45 +0600

    LiveData Observer

---
+++ Base/src/main/java/com/saadahmedsoft/base/utils/lifeCycleOwner.kt
@@ -0,0 +1,17 @@
+package com.saadahmedsoft.base.utils
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+
+inline fun <T> LifecycleOwner.observe(
+    liveData: MutableLiveData<T>,
+    crossinline action: (T) -> Unit
+) {
+    liveData.observe(this) { it?.let(action) }
+}
+
+inline fun <T> LifecycleOwner.observe(liveData: LiveData<T>, crossinline action: (T) -> Unit) {
+    liveData.observe(this) { it?.let(action) }
+}
