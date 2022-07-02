a214d07762d8b7e6d9ec2f399a69677d358641d2
Saad <saadahmedscse@gmail.com>
2022-07-02 23:03:45 +0600

    View functions

---
+++ Base/src/main/java/com/saadahmedsoft/base/utils/view.kt
@@ -0,0 +1,43 @@
+package com.saadahmedsoft.base.utils
+
+import android.app.Activity
+import android.content.Context
+import android.view.View
+import androidx.annotation.ColorRes
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+
+infix fun View.onClicked(onClick: (View) -> Unit) {
+    setOnClickListener { onClick(it) }
+}
+
+infix fun View.onLongPressed(onPress: (View) -> Boolean){
+    setOnLongClickListener {onPress(it)}
+}
+
+fun View.getColor(@ColorRes resId: Int) = ContextCompat.getColor(context, resId)
+fun Context.getColor(@ColorRes resId: Int) = ContextCompat.getColor(this, resId)
+fun Activity.getColor(@ColorRes resId: Int) = ContextCompat.getColor(this, resId)
+fun Fragment.getColor(@ColorRes resId: Int) = ContextCompat.getColor(context!!, resId)
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.enable() {
+    this.isEnabled = true
+    this.alpha = 1f
+}
+
+fun View.disable() {
+    this.isEnabled = false
+    this.alpha = 0.4.toFloat()
+}
