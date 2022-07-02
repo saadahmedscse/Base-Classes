efecfa6d66234ebe76cbce5b19680be1e7056847
Saad <saadahmedscse@gmail.com>
2022-07-02 23:14:45 +0600

    App Constants

---
+++ Base/src/main/java/com/saadahmedsoft/base/utils/Constants.kt
@@ -0,0 +1,13 @@
+package com.saadahmedsoft.base.utils
+
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+
+class Constants {
+    object DURATIONS {
+        const val SNACK_LONG = Snackbar.LENGTH_LONG
+        const val SNACK_SHORT = Snackbar.LENGTH_SHORT
+        const val TOAST_LONG = Toast.LENGTH_LONG
+        const val TOAST_SHORT = Snackbar.LENGTH_SHORT
+    }
+}
