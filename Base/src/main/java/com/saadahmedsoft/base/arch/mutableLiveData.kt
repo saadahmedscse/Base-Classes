53240cd9727333d57921bc2133e45d60998fc35f
Saad <saadahmedscse@gmail.com>
2022-07-02 23:47:02 +0600

    Get mutable liveData of DataState

--- Base/src/main/java/com/saadahmedsoft/base/arch/mutableLiveData.kt
+++ Base/src/main/java/com/saadahmedsoft/base/arch/mutableLiveData.kt
@@ -1,6 +1,7 @@
-package com.saadahmedsoft.base.utils
+package com.saadahmedsoft.base.arch
 
 import androidx.lifecycle.MutableLiveData
+import com.saadahmedsoft.base.utils.DataState
 
 fun<T> getMutableLiveData() : MutableLiveData<DataState<T>> {
     return MutableLiveData<DataState<T>>()
